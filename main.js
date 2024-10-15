05600f63bfafaeb98a56c6ac073cf8a864bb1bfd
KacperSatora <kacpersatora02@gmail.com>
2024-10-15 13:14:06 +0200

    form fill

--- main.js
+++ main.js
@@ -12,11 +12,20 @@ function f1(e) {
         uwagi: document.getElementById("uwagi").value,
         branza: document.getElementById("branza").value,
         checkbox: document.getElementById("btncheck1").value
-    }
+    };
 
-    console.log(formData)
+    console.log(formData);
 }
 
-function f2(e) {
-    e.preventDefault();
+function f2() {
+    document.getElementById("nazwa-firmy").value = "Super Firma";
+    document.getElementById("nip").value = "1122334455";
+    document.getElementById("miasto").value = "Krakow";
+    document.getElementById("ulica").value = "Biezanowska";
+    document.getElementById("numer-domu").value = 5;
+    document.getElementById("number-mieszkania").value = 1;
+    document.getElementById("kod-pocztowy").value = 32020;
+    document.getElementById("uwagi").value = "brak";
+    document.getElementById("branza").value = 1;
+    document.getElementById("btncheck1").value = "true";
 }
