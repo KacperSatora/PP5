5bfd4c70e0f5c0c86537b989062544011fa0d9c2
KacperSatora <kacpersatora02@gmail.com>
2024-10-28 18:15:07 +0100

    form save, clear and client edit start

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
+const customers = []
+
 function f1(e) {
     e.preventDefault();
 
-    const formData = {
+    const customerData = {
         nazwaFirmy: document.getElementById("nazwa-firmy").value,
         NIP: document.getElementById("nip").value,
         miasto: document.getElementById("miasto").value,
         ulica: document.getElementById("ulica").value,
-        numberDomu: document.getElementById("numer-domu").value,
+        numerDomu: document.getElementById("numer-domu").value,
         numerMieszkania: document.getElementById("number-mieszkania").value,
         kodPocztowy: document.getElementById("kod-pocztowy").value,
         uwagi: document.getElementById("uwagi").value,
@@ -14,7 +16,19 @@ function f1(e) {
         checkbox: document.getElementById("btncheck1").value
     };
 
-    console.log(formData);
+    customers.push(customerData)
+    hideForm();
+
+    let ul = document.getElementById("customer-list");
+    ul.innerHTML += `<li class="list-group-item">${customerData.nazwaFirmy}<button class="btn btn-primary" onclick="editField('${customerData.nazwaFirmy}')">Edit</button></li>`;
+
+    console.log(customerData);
+
+
+    var elements = document.getElementById("customer-form");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].value = "";
+    }
 }
 
 function f2() {
@@ -28,4 +42,39 @@ function f2() {
     document.getElementById("uwagi").value = "brak";
     document.getElementById("branza").value = 1;
     document.getElementById("btncheck1").value = "false";
+}
+
+function editField(name) {
+    let intendedCustomer;
+    // operate on array, so that customer can be edited directly instead of pushing one
+    customers.forEach(element => {
+        
+        if (element.nazwaFirmy === name) {
+            intendedCustomer = element;
+        }
+    });
+
+    console.log(intendedCustomer);
+
+    document.getElementById("nazwa-firmy").value = intendedCustomer.nazwaFirmy;
+    document.getElementById("nip").value = intendedCustomer.NIP;
+    document.getElementById("miasto").value = intendedCustomer.miasto;
+    document.getElementById("ulica").value = intendedCustomer.ulica;
+    document.getElementById("numer-domu").value = intendedCustomer.numerDomu;
+    document.getElementById("number-mieszkania").value = intendedCustomer.numerMieszkania;
+    document.getElementById("kod-pocztowy").value = intendedCustomer.kodPocztowy;
+    document.getElementById("uwagi").value = intendedCustomer.uwagi;
+    document.getElementById("branza").value = intendedCustomer.branza;
+    document.getElementById("btncheck1").value = intendedCustomer.checkbox;
+    showForm();
+}
+
+function showForm() {
+    document.getElementById('customer-form').style.display = 'block'
+    document.getElementById('customer-list').style.display = 'none'
+}
+
+function hideForm() {
+    document.getElementById('customer-form').style.display = 'none'
+    document.getElementById('customer-list').style.display = 'block'
 }
